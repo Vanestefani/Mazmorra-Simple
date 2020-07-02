ea9326e843989ba4c068b24f0e9abdf20d3b40cc
vanessa <vanessa.estefania.corredor@gmail.com>
2020-07-02 18:53:32 -0500

    victoria y obtener llave

--- js/juego.js
+++ js/juego.js
@@ -5,134 +5,158 @@ var FPS = 50;
 var anchoF = 50;
 var altoF = 50;
 
-var muro = '#044f14';
-var puerta = '#3a1700';
-var tierra = '#c6892f';
-var llave = '#eff542';
+var muro = "#044f14";
+var puerta = "#3a1700";
+var tierra = "#c6892f";
+var llave = "#eff542";
 
 var protagonista;
 
 var escenario = [
-  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-  [0,2,2,0,0,0,2,2,2,2,0,0,2,2,0],
-  [0,0,2,2,2,2,2,0,0,2,0,0,2,0,0],
-  [0,0,2,0,0,0,2,2,0,2,2,2,2,0,0],
-  [0,0,2,2,2,0,0,2,0,0,0,2,0,0,0],
-  [0,2,2,0,0,0,0,2,0,0,0,2,0,0,0],
-  [0,0,2,0,0,0,2,2,2,0,0,2,2,2,0],
-  [0,2,2,2,0,0,2,0,0,0,1,0,0,2,0],
-  [0,2,2,3,0,0,2,0,0,2,2,2,2,2,0],
-  [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-]
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 2, 2, 0, 0, 0, 2, 2, 2, 2, 0, 0, 2, 2, 0],
+  [0, 0, 2, 2, 2, 2, 2, 0, 0, 2, 0, 0, 2, 0, 0],
+  [0, 0, 2, 0, 0, 0, 2, 2, 0, 2, 2, 2, 2, 0, 0],
+  [0, 0, 2, 2, 2, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0],
+  [0, 2, 2, 0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0],
+  [0, 0, 2, 0, 0, 0, 2, 2, 2, 0, 0, 2, 2, 2, 0],
+  [0, 2, 2, 2, 0, 0, 2, 0, 0, 0, 1, 0, 0, 2, 0],
+  [0, 2, 2, 3, 0, 0, 2, 0, 0, 2, 2, 2, 2, 2, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
 
-function dibujaEscenario(){
+function dibujaEscenario() {
   var color;
 
-  for(y=0;y<10;y++){
-    for(x=0;x<15;x++){
+  for (y = 0; y < 10; y++) {
+    for (x = 0; x < 15; x++) {
+      if (escenario[y][x] == 0) color = muro;
 
-      if(escenario[y][x]==0)
-        color = muro;
+      if (escenario[y][x] == 1) color = puerta;
 
-      if(escenario[y][x]==1)
-        color = puerta;
+      if (escenario[y][x] == 2) color = tierra;
 
-      if(escenario[y][x]==2)
-        color = tierra;
-
-      if(escenario[y][x]==3)
-        color = llave;
+      if (escenario[y][x] == 3) color = llave;
 
       ctx.fillStyle = color;
-      ctx.fillRect(x*anchoF,y*altoF,anchoF,altoF);
+      ctx.fillRect(x * anchoF, y * altoF, anchoF, altoF);
     }
   }
-
 }
 
 //OBJETO JUGADOR
-var jugador = function(){
+var jugador = function () {
   this.x = 1;
   this.y = 1;
 
-  this.color = '#820c01';
-
-  this.dibuja = function(){
+  this.color = "#820c01";
+  this.llave = "#eff542";
+  this.dibuja = function () {
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x*anchoF,this.y*altoF,anchoF,altoF);
-  }
+    ctx.fillRect(this.x * anchoF, this.y * altoF, anchoF, altoF);
+  };
 
-  this.margenes = function(x,y){
+  this.margenes = function (x, y) {
     var colision = false;
 
-    if(escenario[y][x]==0){
+    if (escenario[y][x] == 0) {
       colision = true;
     }
 
-    return(colision);
-  }
+    return colision;
+  };
 
-  this.arriba = function(){
-    if(this.margenes(this.x, this.y-1)==false)
+  this.arriba = function () {
+    if (this.margenes(this.x, this.y - 1) == false) {
       this.y--;
-  }
+      this.logicaObjetos();
+    }
+  };
 
-  this.abajo = function(){
-    if(this.margenes(this.x, this.y+1)==false)
+  this.abajo = function () {
+    if (this.margenes(this.x, this.y + 1) == false) {
       this.y++;
-  }
+      this.logicaObjetos();
+    }
+  };
 
-  this.izquierda = function(){
-    if(this.margenes(this.x-1, this.y)==false)
+  this.izquierda = function () {
+    if (this.margenes(this.x - 1, this.y) == false) {
       this.x--;
-  }
+      this.logicaObjetos();
+    }
+  };
 
-  this.derecha = function(){
-    if(this.margenes(this.x+1, this.y)==false)
+  this.derecha = function () {
+    if (this.margenes(this.x + 1, this.y) == false) {
       this.x++;
-  }
+      this.logicaObjetos();
+    }
+  };
+  this.victoria = function () {
+    this.x = 1;
+    this.y = 1;
+    this.llave = false;
+    escenario[8][3] = 3;
+    console.log("Haz ganado ")
 
-}
+  };
+  this.logicaObjetos = function () {
+    var objeto = escenario[this.y][this.x];
+    if (objeto == 3) {
+      this.llave = true;
+      escenario[this.y][this.x] = 2;
+      console.log("Ahora tienes la llave ")
 
-function inicializa(){
-  canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
+    }
+    if (objeto == 1 ) {
+      if( this.llave==true){
+        this.victoria();
+      }else{
+        console.log("te falta la llave no puedes pasar ")
+      }
+
+    }
+  };
+};
+
+function inicializa() {
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
 
   //CREAMOS AL JUGADOR
   protagonista = new jugador();
 
   //LECTURA DEL TECLADO
-  document.addEventListener('keydown',function(tecla){
-
-    if(tecla.keyCode == 38){
+  document.addEventListener("keydown", function (tecla) {
+    if (tecla.keyCode == 38) {
       protagonista.arriba();
     }
 
-    if(tecla.keyCode == 40){
+    if (tecla.keyCode == 40) {
       protagonista.abajo();
     }
 
-    if(tecla.keyCode == 37){
+    if (tecla.keyCode == 37) {
       protagonista.izquierda();
     }
 
-    if(tecla.keyCode == 39){
+    if (tecla.keyCode == 39) {
       protagonista.derecha();
     }
-
   });
 
-  setInterval(function(){
+  setInterval(function () {
     principal();
-  },1000/FPS);
+  }, 1000 / FPS);
 }
 
-function borraCanvas(){
-  canvas.width=750;
-  canvas.height=500;
+function borraCanvas() {
+  canvas.width = 750;
+  canvas.height = 500;
 }
 
-function principal(){
+function principal() {
   borraCanvas();
   dibujaEscenario();
   protagonista.dibuja();
